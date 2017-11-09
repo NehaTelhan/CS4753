397c7eabea9b8f5fd2b8b9f465e7482728507c7d
Sadiyah Faruk <sf2ne@virginia.edu>
2017-11-09 17:53:41 -0500

    Added functionality to add user to subscription list

--- snow-web/web/footer.php
+++ snow-web/web/footer.php
@@ -1,15 +1,19 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-function test_input($data) {
+function test_input2($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
 
-function test_email($data) {
+function test_email2($data) {
   if (strpos($data, ".") !== false) {
     return True;
   } else {
@@ -21,35 +25,78 @@ include_once('connect.php');
 
 $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 // Check connection
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 // Form the SQL query (an INSERT query)
-if(isset($_POST['name'])){
-  $email = test_input($_POST['email']);
-  $_SESSION["Email"] = $email;
-  if (test_email($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $sql="INSERT INTO contact_requests (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
-    if (!mysqli_query($con,$sql)){
-        die('Error: ' . mysqli_error($con));
-        $fmsg ="Contact request failed";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //something posted
+    if (isset($_POST['btnSubscribe'])) {
+        // btnSubscribe
+        $email = $_POST['email'];
+        $_SESSION["Email"] = $email;
+        if (test_email2($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $sql="UPDATE USERS SET subscribed='1' WHERE email='$email'";
+          $name="SELECT name FROM `Users` WHERE email='$email'";
+          if (!mysqli_query($con,$sql)){
+              die('Error: ' . mysqli_error($con));
+              $fmsg ="Contact request failed";
 
-    } else {
-        // echo "Welcome $name. You are registered as $email.";
-        $smsg = "Thanks for the contacting us, $name. One of our representatives will get back to you at: $email. Click <a href='index.php'>here</a> to return to the home page.";
+          } else {
+              // echo "Welcome $name. You are registered as $email.";
+              $smsg = "$email subscribed to mailing list";
+              send_email2($email, $name);
+          }
+        } else {
+          $fmsg = "Subscription request failed:";
+          if(!test_email2($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $fmsg .=" Improperly Formatted Email ";
+
+          }
+
+        }
+        mysqli_close($con);
     }
-  } else {
-    $fmsg = "Contact request failed:";
-    if(!test_email($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $fmsg .=" Improperly Formatted Email ";
-
-    }
-
   }
-  mysqli_close($con);
-}
 
+    function send_email2($email, $name) {
+      $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+      try {
+          //Server settings
+          $mail->SMTPDebug = false;                                 // Enable verbose debug output
+          $mail->isSMTP();                                      // Set mailer to use SMTP
+          $mail->Host = 'smtp.gmail.com'; ;  // Specify main and backup SMTP servers
+          $mail->SMTPAuth = true;                               // Enable SMTP authentication
+          $mail->Username = 'chaiwalli12@gmail.com';                 // SMTP username
+          $mail->Password = 'welovechai$$$$';                           // SMTP password
+          $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+          $mail->Port = 587;                                    // TCP port to connect to
+
+          //Recipients
+          $mail->setFrom('chaiwalli12@gmail.com', 'Sadiyah from Chai Walli');
+          $mail->addAddress($email, $name);     // Add a recipient
+          // $mail->addAddress('ellen@example.com');               // Name is optional
+          // $mail->addReplyTo('info@example.com', 'Information');
+          // $mail->addCC('cc@example.com');
+          // $mail->addBCC('bcc@example.com');
+
+          //Attachments
+          // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+          // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+          //Content
+          $mail->isHTML(true);                                  // Set email format to HTML
+          $mail->Subject = 'Subscription Confirmed';
+          $mail->Body    = 'Thank you so much for subscribing! Look forward to our tea themed emails!';
+          $mail->AltBody = 'Thank you so much for subscribing! Look forward to our tea themed emails!';
+
+          $mail->send();
+          // echo 'Message has been sent';
+      } catch (Exception $e) {
+          // echo 'Message could not be sent.';
+          // echo 'Mailer Error: ' . $mail->ErrorInfo;
+      }
+    }
 
  ?>
 
@@ -83,8 +130,8 @@ if(isset($_POST['name'])){
              <form>
                <div class="form-group">
                  <input style="font-size: 60%" name="email" id="inputEmail" type="text" value="Enter your email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email';}">
-               </div>
-            <input style="font-size: 70%" type="submit" value="Go">
+               <button name="btnSubscribe" class="btn btn-small" formmethod="post" style="color:white; background:black" type="submit">Subscribe</button>
+             </div>
              </form>
             </div>
         <ul class="social">
